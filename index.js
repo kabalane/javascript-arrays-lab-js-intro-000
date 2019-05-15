a5f72264775da068f90fb57b01e48229f197f80a
Kabalan Elahmadie <kahmadieh@yahoo.com>
2019-05-15 20:08:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 function destructivelyAppendKitten(name){
   kittens.push(name);
 }
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+}
+function destructivelyRemoveLastKitten(name){
+  kittens.pop(name);
+}
+
